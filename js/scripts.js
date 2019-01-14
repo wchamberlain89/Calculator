a95f326ebc3cbf051ecb939fcb1ef7eb65482564
omar kadah and ward chamberlain <omar@>
2019-01-14 13:30:28 -0800

    Created Directory

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2){
+	return number1 + number2;
+}
+
+var num1 = parseInt(prompt("Enter a Number"));
+var num2 = parseInt(prompt("Enter a second number"));
+
+alert(add(num1, num2));
