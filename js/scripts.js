ec4d479a2dbee3b394fd7726962d31c543bdbb19
omar kadah and ward chamberlain <omar@>
2019-01-14 15:19:53 -0800

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,46 @@
-var add = function(number1, number2){
-	return number1 + number2;
+var add = function(num1, num2){
+	return num1 + num2;
 }
+
+function add (num1, num2){
+	return num1 + num2;
+}
+
+function subtract(num1, num2){
+	return num1 - num2;
+}
+
+function multiply (num1, num2){
+	return num1 * num2;
+}
+
+function divide (num1, num2){
+	return num1 / num2;
+}
+
+function bmi (wgt, hgt){
+	return wgt / (hgt*hgt) * 703;
+}
+
+function tempConvert (fahrenheit) {
+	return (fahrenheit - 32) * (5/9);
+}
+
+alert (tempConvert (73))
+alert (bmi (185, 69));
 
 var num1 = parseInt(prompt("Enter a Number"));
 var num2 = parseInt(prompt("Enter a second number"));
 
-alert(add(num1, num2));
+result = add(num1, num2);
+result4 = subtract(num1, num2);
+result2 = multiply(num1, num2);
+result3 = divide(num1, num2);
+
+// alert(result);
+// alert(result4);
+// alert(result2);
+// alert(result3);
+
+
+alert("Addition: " + result + " Subtract: " + result4 + " Multiply: " + result2 + " Divide: " + result3);
