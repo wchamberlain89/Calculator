fe4aafbfadbc38d21ddd45344833b2a591f9a34f
Aurora Shido-Wagenet and Ward Chamberlain <aurora+ward@>
2019-01-16 11:45:10 -0800

    add addition functionality and forms for other functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-var add = function(num1, num2){
-	return num1 + num2;
-}
-
 function add (num1, num2){
 	return num1 + num2;
 }
@@ -26,21 +22,12 @@ function tempConvert (fahrenheit) {
 	return (fahrenheit - 32) * (5/9);
 }
 
-alert (tempConvert (73))
-alert (bmi (185, 69));
+$(function(){
+	$("button").click(function(event){
+		var addinput1 = parseInt($("#add1").val());
+		var addinput2 = parseInt($("#add2").val());
 
-var num1 = parseInt(prompt("Enter a Number"));
-var num2 = parseInt(prompt("Enter a second number"));
-
-result = add(num1, num2);
-result4 = subtract(num1, num2);
-result2 = multiply(num1, num2);
-result3 = divide(num1, num2);
-
-// alert(result);
-// alert(result4);
-// alert(result2);
-// alert(result3);
-
-
-alert("Addition: " + result + " Subtract: " + result4 + " Multiply: " + result2 + " Divide: " + result3);
+		$(".result h3").text(add(addinput1,addinput2));
+		event.preventDefault();
+	});
+});
